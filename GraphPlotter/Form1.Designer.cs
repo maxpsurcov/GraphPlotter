b39ce0f8c4e445cb251a287d42a787723da3eec0
maxpsurcov <max.psurcov1@gmail.com>
2023-04-21 16:25:08 +0300

    Добавьте файлы проекта.

---
+++ GraphPlotter/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace GraphPlotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(583, 193);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 419);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(712, 63);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(135, 29);
+            this.txtR.TabIndex = 1;
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(539, 63);
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(135, 29);
+            this.txtH.TabIndex = 2;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(267, 125);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(161, 56);
+            this.btnDraw.TabIndex = 3;
+            this.btnDraw.Text = "button1";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1373, 790);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.txtH);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ GraphPlotter/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphPlotter
+{
+    public partial class Form1 : Form
+    {
+        private double r, h;
+        private double minX, maxX, minY, maxY;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            // Зчитування введених користувачем значень
+            if (!double.TryParse(txtR.Text, out r))
+            {
+                MessageBox.Show("Невірний формат значення R!");
+                return;
+            }
+
+            if (!double.TryParse(txtH.Text, out h))
+            {
+                MessageBox.Show("Невірний формат значення H!");
+                return;
+            }
+
+            // Визначення області визначення функції
+            minX = -Math.PI;
+            maxX = Math.PI;
+            minY = r - h;
+            maxY = r + h;
+
+            // Очистка панелі для графіку
+            panel1.Refresh();
+
+            // Створення графіку
+            Graphics g = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Blue, 2);
+
+            // Відображення системи координат
+            DrawAxes(g);
+
+            // Побудова графіку функції
+            DrawGraph(g, pen);
+        }
+
+        private void DrawAxes(Graphics g)
+        {
+            // Визначення розмірів панелі
+            int width = panel1.Width;
+            int height = panel1.Height;
+
+            // Відображення осей координат
+            g.DrawLine(Pens.Black, 0, height / 2, width, height / 2);
+            g.DrawLine(Pens.Black, width / 2, 0, width / 2, height);
+
+            // Відображення підписів осей координат
+            Font font = new Font("Arial", 10);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+
+            g.DrawString("X", font, Brushes.Black, width - 20, height / 2 - 20, format);
+            g.DrawString("Y", font, Brushes.Black, width / 2 + 20, 0, format);
+
+            // Відображення поділок на осі X
+            double stepX = (maxX - minX) / 10;
+
+            for (double x = minX; x <= maxX; x += stepX)
+            {
+                int posX = (int)((x - minX) / (maxX - minX) * width);
+
+                g.DrawLine(Pens.Black, posX, height / 2 - 5, posX, height / 2 + 5);
+                g.DrawString(x.ToString("0.##"), font, Brushes.Black, posX, height / 2 + 5, format);
+            }
+
+            // Відображення поділок на осі Y
+            double stepY = (maxY - minY) / 10;
+
+            for (double y = minY; y <= maxY; y += stepY)
+            {
+                int posY = (int)((maxY - y) / (maxY * minY) * height);
+                g.DrawLine(Pens.Black, width / 2 - 5, posY, width / 2 + 5, posY);
+                g.DrawString(y.ToString("0.##"), font, Brushes.Black, width / 2 + 10, posY - 10, format);
+            }
+        }
+        private void DrawGraph(Graphics g, Pen pen)
+        {
+            // Визначення кроку для побудови кривої
+            double step = 0.1;
+
+            // Обчислення координат точок кривої
+            for (double t = 0; t <= Math.PI * 2; t += step)
+            {
+                double x = r * t - h * Math.Sin(t);
+                double y = r - h * Math.Cos(t);
+
+                // Перетворення координат до системи координат панелі
+                int posX = (int)((x - minX) / (maxX - minX) * panel1.Width);
+                int posY = (int)((maxY - y) / (maxY - minY) * panel1.Height);
+
+                // Відображення точки на графіку
+                g.DrawEllipse(pen, posX - 2, posY - 2, 4, 4);
+            }
+        }
+    }
+}
